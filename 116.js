2042a51bb06a3d1be53e0b12326ec891b5d4bea3
mualado104 <mualado107@gmail.com>
2016-11-07 13:00:26 +0700

    Create 116.js

---
+++ 116.js
@@ -0,0 +1,10 @@
+function passCustom(){ 
+	simpleCart.checkoutTo = Custom;
+	simpleCart.email = &quot;mualado103@gmail.com&quot;
+	simpleCart.currency = VND;
+	simpleCart.successUrl = &quot;http://www.smartphonethongminh.top/&quot;
+} 
+function passGoogle(){ 
+	simpleCart.checkoutTo = Email; 		
+	simpleCart.currency = VND;
+}
